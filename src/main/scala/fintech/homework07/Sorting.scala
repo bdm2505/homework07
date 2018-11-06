d3608299aac1f5ed627cd3005e02293c15c62e4c
bdm <bdm1999@yandex.ru>
2018-11-06 18:37:43 +0500

    mergeSort and quickSort

--- src/main/scala/fintech/homework07/Sorting.scala
+++ src/main/scala/fintech/homework07/Sorting.scala
@@ -1,14 +1,125 @@
 package fintech.homework07
 
+import scala.collection.mutable
+import scala.math.Ordering
+
+
 /**
   * Реализовать алгоритмы quick-sort и merge-sort
   *  использую *подходящие* *мутабельные* коллекции
   */
 
-object Sorting {
+object Sorting extends App {
+  type Arr[T] = mutable.IndexedSeq[T]
 
-//  def mergeSort(???)
-//
-//  def quickSort(???)
+  implicit class SortingOpsForMutable[T](val seq: Arr[T]) {
+    def mergeSort(implicit ord: Ordering[T]): Sorting.Arr[T] = Sorting.mergeSort(seq)(ord)
+
+    def quickSort(implicit ord: Ordering[T]): Sorting.Arr[T] = Sorting.quickSort(seq)(ord)
+  }
+
+  implicit class SortingOpsForImmutable[T](val seq: Seq[T]) {
+    def mergeSort(implicit ord: Ordering[T]): Seq[T] = Sorting.mergeSort(new mutable.ArrayBuffer[T] ++= seq)(ord)
+
+    def quickSort(implicit ord: Ordering[T]): Seq[T] = Sorting.quickSort(new mutable.ArrayBuffer[T] ++= seq)(ord)
+  }
+
+
+  def mergeSort[T](seq: Arr[T])(implicit ord: Ordering[T]): Arr[T] = {
+    if (seq.size > 1) {
+      val c = seq.size / 2
+      val left = mergeSort(seq.take(c))
+      val right = mergeSort(seq.drop(c))
+      var l, r, index = 0
+      while (l < left.size && r < right.size) {
+        if (ord.compare(left(l), right(r)) < 0) {
+          seq(index) = left(l)
+          l += 1
+        } else {
+          seq(index) = right(r)
+          r += 1
+        }
+        index += 1
+      }
+      while (l < left.size) {
+        seq(index) = left(l)
+        l += 1
+        index += 1
+      }
+      while (r < right.size) {
+        seq(index) = right(r)
+        r += 1
+        index += 1
+      }
+      seq
+    }
+    else
+      seq
+  }
+
+
+  def quickSort[K](seq: Arr[K])(implicit ord: Ordering[K]): Arr[K] = {
+
+    def quick(left: Int, right: Int): Unit = {
+      if (right - left > 1) {
+        var center = (left + right) / 2
+        val p = seq(center)
+        var iRight = center + 1
+        var iLeft = left
+        var r = right
+        while (center - iLeft > 0) {
+          val current = seq(iLeft)
+          ord.compare(current, p) match {
+            case 0 =>
+              seq(iLeft) = seq(center - 1)
+              seq(center - 1) = current
+              center -= 1
+            case x if x < 0 =>
+              iLeft += 1
+            case _ if r > iRight =>
+              seq(iLeft) = seq(r - 1)
+              seq(r - 1) = current
+              r -= 1
+            case _ =>
+              seq(iLeft) = seq(center - 1)
+              seq(center - 1) = seq(iRight - 1)
+              seq(iRight - 1) = current
+              center -= 1
+              iRight -= 1
+              r -= 1
+          }
+        }
+        while (r - iRight > 0) {
+          val current = seq(r - 1)
+          ord.compare(current, p) match {
+            case 0 =>
+              seq(r - 1) = seq(iRight)
+              seq(iRight) = current
+              iRight += 1
+            case x if x > 0 =>
+              r -= 1
+            case _ =>
+              seq(r - 1) = seq(iRight)
+              seq(iRight) = seq(center)
+              seq(center) = current
+              iLeft += 1
+              center += 1
+              iRight += 1
+          }
+        }
+        if (iLeft - left < right - r) {
+          quick(left, iLeft)
+          quick(r, right)
+        }
+        else {
+          quick(r, right)
+          quick(left, iLeft)
+        }
+      }
+    }
+
+    quick(0, seq.length)
+    seq
+  }
 
 }
--- src/test/scala/fintech/homework07/SortingSpec.scala
+++ src/test/scala/fintech/homework07/SortingSpec.scala
@@ -1,6 +1,29 @@
 package fintech.homework07
+
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.util.Random
+
 class SortingSpec extends FlatSpec with Matchers {
-  ???
+
+  import Sorting._
+
+  trait A {
+    val randomArray: Seq[Double] = 1 to 100 map (_ => Random.nextDouble())
+
+    def isSorted(arr: Seq[Double]): Boolean = arr.init zip arr.tail forall { case (v1, v2) => v1 <= v2 }
+
+    val simple: Seq[Int] = Seq(2, 4, 1, 0, 3).sorted
+    val sortedSimple = Seq(0, 1, 2, 3, 4)
+  }
+
+  it should "mergeSort is correctly" in new A {
+    isSorted(randomArray.mergeSort) shouldBe true
+    simple.mergeSort shouldBe sortedSimple
+  }
+
+  it should "quickSort is correctly" in new A {
+    isSorted(randomArray.quickSort) shouldBe true
+    simple.quickSort shouldBe sortedSimple
+  }
 }
